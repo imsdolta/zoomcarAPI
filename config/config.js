7d0ea4132a36ac7455d6f7ecddc28c640f8add40
imsdolta <imsdolta@gmail.com>
2020-11-06 01:09:02 +0530

    DSf

--- config/config.js
+++ config/config.js
@@ -3,5 +3,8 @@ module.exports = {
         port: process.env.PORT || 3000,
         dbPath: 'mongodb://localhost:27017/zoomcarAPI'
     },
-    production: {}
+    production: {
+        dbPath: '',
+        PORT: 8080
+    }
 };
